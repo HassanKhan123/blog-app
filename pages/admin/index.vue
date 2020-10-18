a0acbeeb66356cd50f4b1bfa59185843c9699fea
HassanKhan123 <hk2602809@gmail.com>
2020-10-18 16:07:57 +0500

    admin auth page added

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="admin-page">
     <section class="new-post">
-        <button @click="$router.push('/admin/new-post')">Create Post</button>
+        <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton>
     </section>
 
     <section class="existing-posts">
@@ -13,10 +13,12 @@
 </template>
 
 <script>
+import AppButton from "@/components/UI/AppButton";
 import PostsList from "@/components/Posts/PostsList";
 export default {
     components: {
         PostsList,
+        AppButton
     },
 };
 </script>
