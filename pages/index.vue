2c69891f3d951e313c74a604fa95deaaa6f0adaf
HassanKhan123 <hk2602809@gmail.com>
2020-10-18 13:25:36 +0500

    postPreview component added

--- pages/index.vue
+++ pages/index.vue
@@ -4,32 +4,20 @@
         <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-        <nuxt-link :to="'/posts/' + 1" class="post-preview">
-            <article>
-                <div class="post-thumbnail" style="
-              background-image: url('https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg');
-            "></div>
-                <div class="post-content">
-                    <h1>Post Text</h1>
-                    <p>Preview Text</p>
-                </div>
-            </article>
-        </nuxt-link>
-
-        <nuxt-link :to="'/posts/' + 2" class="post-preview">
-            <article>
-                <div class="post-thumbnail" style="
-              background-image: url('https://sm.mashable.com/mashable_pk/photo/default/tech_ntqz.jpg');
-            "></div>
-                <div class="post-content">
-                    <h1>Post Text 2</h1>
-                    <p>Preview Text 2</p>
-                </div>
-            </article>
-        </nuxt-link>
+        <PostPreview id="1" title="Post Title" previewText="Post Content" thumbnail="https://sm.mashable.com/mashable_pk/photo/default/tech_ntqz.jpg" />
+        <PostPreview id="2" title="Post Title 2" previewText="Post Content 2" thumbnail="https://sm.mashable.com/mashable_pk/photo/default/tech_ntqz.jpg" />
     </section>
 </div>
 </template>
+
+<script>
+import PostPreview from "../components/Posts/PostPreview";
+export default {
+    components: {
+        PostPreview,
+    },
+};
+</script>
 
 <style scoped>
 .intro {
@@ -69,41 +57,5 @@
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
-}
-
-.post-preview {
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 2px #ccc;
-    background-color: white;
-    width: 90%;
-}
-
-a {
-    text-decoration: none;
-    color: black;
-}
-
-@media (min-width: 850px) {
-    .post-preview {
-        width: 400px;
-        margin: 10px;
-    }
-}
-
-.post-thumbnail {
-    width: 100%;
-    height: 200px;
-    background-position: center;
-    background-size: cover;
-}
-
-.post-content {
-    padding: 10px;
-    text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-    background-color: #ccc;
 }
 </style>
