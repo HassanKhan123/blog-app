8551a0678703c1a7479c478fe2b3b7669636d40e
HassanKhan123 <hk2602809@gmail.com>
2020-10-19 06:59:14 +0500

    fetching data from the backend

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from "vuex";
+import axios from 'axios'
 
 const createStore = () => {
   return new Vuex.Store({
@@ -12,27 +13,13 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit("setPosts", [
-              {
-                id: "1",
-                title: "Post Title",
-                previewText: "Post content",
-                thumbnail:
-                  "https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg"
-              },
-              {
-                id: "2",
-                title: "Post Title",
-                previewText: "Post content",
-                thumbnail:
-                  "https://s27389.pcdn.co/wp-content/uploads/2019/10/retail-innovation-changing-tech-consumer-employee-demands-1024x440.jpeg"
-              }
-            ]);
-            resolve();
-          }, 1500);
-        })
+        return axios.get('https://nuxt-blog-55f05.firebaseio.com/posts.json').then(res => {
+          const postsArray=[];
+          for(const key in res.data){
+            postsArray.push({...res.data[key],id:key})
+          }
+          vuexContext.commit('setPosts',postsArray)
+        }).catch(err => context.error(err))
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit("setPosts", posts);
